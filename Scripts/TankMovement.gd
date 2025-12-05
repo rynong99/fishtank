203e45d0546f74bd4135a00f607b636c8c76c230
Pipervetowich <111729427+Pipervetowich@users.noreply.github.com>
2025-12-05 12:52:20 -0700

    Revert "Merge branch 'main' into Piper"
    This reverts commit 8d7dcf15d02805ce9be8fe0f89836677b76cc223, reversing
    changes made to 56ae148fe64a604d352098b460cc5d2b57280966.

--- Scripts/TankMovement.gd
+++ Scripts/TankMovement.gd
@@ -4,30 +4,32 @@ var speed : float = 0
 @export var acceleration : float = 0.1
 @export var turn_rate : float = 1
 var tank_direction = DirectionController.direction
+var prev_td = DirectionController.direction
 var tank_rotation = DirectionController.rotation
 
 var total_distance : float
 var progress_bar : ProgressBar
 var progress : PathFollow2D
 var speedometer : TextureProgressBar
-
 	
 func _ready() -> void:
+	progress_bar = find_parent("OutsideViewer").find_child("ProgressBar") # Find the progress bar
+	speedometer = find_parent("OutsideViewer").find_child("Speedometer") # Find the speedometer
+	
 	# Get the total distance to finish the course
 	#total_distance = %Start_Line.get_child(0).position.distance_to(%Finish_Line.get_child(0).position)
 	#print(total_distance)
-	progress_bar = find_parent("OutsideViewer").find_child("ProgressBar")
 	#progress = %Progress
-	print(progress_bar.value)
-	speedometer = find_parent("OutsideViewer").find_child("Speedometer")
 
 func _process(_delta: float) -> void:
+	# Update progress bar
+	%Progress.progress = %Path.get_curve().get_baked_length() + %Tank.position.y 
+	progress_bar.value = remap(%Progress.progress_ratio,0,1,100,0)
+	
 	#var progress = position.distance_to(%Finish_Line.get_child(0).position)
 	#print(progress) 
 	#progress_bar.value = remap(progress, total_distance, 0, 0, 100.0)
-	%Progress.progress = %Path.get_curve().get_baked_length() + %Tank.position.y
-	progress_bar.value = remap(%Progress.progress_ratio,0,1,100,0)
-	game_over()
+	#print(progress_bar.value)
 
 func _physics_process(delta: float) -> void:
 	get_tank_direction()
@@ -38,36 +40,48 @@ func _physics_process(delta: float) -> void:
 		rotation -= turn_rate*delta/5
 	elif tank_rotation == "Right":
 		rotation += turn_rate*delta/5
+	
 	if tank_direction == "Forward":
 		if speed <= max_speed:
 			speed += acceleration
+		if prev_td != "Forward":
+			speed = 0
 		velocity = speed * direction
 	elif tank_direction == "Reverse":
 		if speed <= max_speed:
 			speed += acceleration
+		if prev_td != "Reverse":
+			speed = 0
 		velocity = -speed * direction
 	elif tank_direction == "Stopped":
-		if speed != 0:
-			speed -= acceleration/7.5
+		if speed > 0:
+			speed -= acceleration
+			var tv : Vector2
 			if velocity < Vector2.ZERO:
-				velocity = -speed * direction
+				tv = -speed * direction.abs()
 			elif velocity > Vector2.ZERO:
-				velocity = speed * direction
+				tv = speed * direction.abs()
+			velocity = tv
 		else:
 			velocity = Vector2.ZERO
+	
 	var collision = move_and_collide(velocity*delta)
-	if collision and abs(speed) >= max_speed:
+	if collision and abs(speed) >= max_speed: # ???
 		if not DirectionController.crashed:
+			DirectionController.running = false
 			DirectionController.crashed = true
 			speed = 0 
+	else:
+		DirectionController.crashed = false
 	
-	speedometer.value = remap(speed, 0, max_speed, 0 , 100.0)
+	speedometer.value = remap(speed, 0, max_speed, 0 , 100.0) # Update Speedometer
+	
+	# Speed acts weird... why does the tank not
+	# move and yet the speed and velocity are 
+	# not at 0?
 
 func get_tank_direction():
+	prev_td = tank_direction
 	tank_direction = DirectionController.direction
 func get_tank_rotation():
 	tank_rotation = DirectionController.rotation
-
-func game_over():
-	if progress_bar.value >= 99:
-		GameVar.finished = true
